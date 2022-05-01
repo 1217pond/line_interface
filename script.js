767def9665619bdbd7bbb4889f5a08afada851c2
1217pond <83156814+1217pond@users.noreply.github.com>
2022-05-01 21:09:16 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ async function login(){//ログイン処理
         Cookies.set('pass', pass, {expires: 7});
         Cookies.set('account_name', account_name, {expires: 7});
         document.getElementById('loginArea').remove();
-        token = decrypt(encrypted_token,main_pass);
         desting();
       }else{
         console.log("login canceled.");
@@ -103,74 +102,158 @@ async function chat(e){//チャット表示
   }
 }
 
-function image_sender(){
+function file_sender(){
   img_selected = false;
-  sendArea.innerHTML = '<input type="file" id="img_selector" accept="image/*"><br><input type="button" id="send_button" value="送信"><input type="button" id="text_button" value="テキスト"><input type="button" id="icon_button" value="アイコン変更" disabled><br>';
-  document.getElementById("send_button").addEventListener("click",image_send);
+  sendArea.innerHTML = '<input type="file" id="file_selector"><br><input type="button" id="send_button" value="送信"><input type="button" id="text_button" value="テキスト"><input type="button" id="icon_button" value="アイコン変更"><br>';
+  document.getElementById("send_button").addEventListener("click",file_send);
   document.getElementById("text_button").addEventListener("click",reset_sendArea);
   document.getElementById("icon_button").addEventListener("click",icon_updater);
-  document.getElementById("img_selector").addEventListener("change",preview_img);
 }
 
-function preview_img(){
-  img_selected = true;
-  let preview = document.createElement('img');
-  preview.height = 400;
-  preview.id = "img_src";
-  let file = document.getElementById("img_selector").files[0];
+async function file_send(){
+  chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>ファイルの前処理中...`;
+  let file = document.getElementById("file_selector").files[0];
   let reader = new FileReader();
-  reader.addEventListener("load",function(){
-    preview.src = reader.result;
-    sendArea.appendChild(preview);
+  reader.addEventListener("load",async () => {
+    let data = reader.result;//.substr(22);
+    let type = file.type.split("/");
+    let extension = file.name.split(".")[1];
+    let sub = "None";
+    try{
+      switch(type[0]){
+        case "image":
+          let canvas = document.createElement("canvas");
+          let img = document.createElement("img");
+          img.src = data;
+          await wait_load_img(img);
+          canvas.width = img.width;
+          canvas.height = img.height;
+          let ctx = canvas.getContext("2d");
+          ctx.drawImage(img,0,0);
+          data = canvas.toDataURL("image/jpeg");
+          let x = Math.sqrt(160000/(img.width*img.height));
+          canvas.width = Math.floor(x*canvas.width);
+          canvas.height = Math.floor(x*canvas.height);
+          ctx = canvas.getContext("2d");
+          ctx.drawImage(img,0,0,canvas.width,canvas.height);
+          sub = DataUrl_sep(canvas.toDataURL("image/jpeg"))[1];
+          break;
+        case "video":
+          if(type[1] == "mp4"){
+            let video = document.createElement("video");
+            video.src = data;
+            video.autoplay = true;
+            await wait_load_video(video);
+            let canvas = document.createElement("canvas");
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            canvas.getContext("2d").drawImage(video, 0, 0);
+            sub = DataUrl_sep(canvas.toDataURL("image/jpeg"))[1];
+          }else{
+            let res = await wait_select("動画ファイルの形式がmp4でないためLINE上では動画メッセージとして表示されません。そのまま送信しますか？");
+            if(res == "cancel"){
+              throw "取り消しました。";
+            }
+          }
+          break;
+        case "audio":
+          if(type[1] == "aac" || type[1] == "x-m4a"){
+            let audio = document.createElement("audio");
+            audio.src = data;
+            await wait_load_video(audio);
+            sub = String(Math.round(audio.duration*1000));
+          }else{
+            let res = await wait_select("音声ファイルの形式がm4aでないためLINE上では音声メッセージとして表示されません。そのまま送信しますか？");
+            if(res == "cancel"){
+              throw "取り消しました。";
+            }
+          }
+          break;
+      }
+      let uri_prop = DataUrl_sep(data);
+      chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>ファイル送信中...`;
+      await request({
+        "use":"upload",
+        "pass":main_pass,
+        "mime":uri_prop[0],
+        "data":uri_prop[1],
+        "ext":extension,
+        "sub":sub,
+        "sender":JSON.stringify({
+          "name":account_name,
+          "iconUrl":my_icon_url
+        }),
+        "user_id":account_id,
+        "room_id":room_id,
+        "crossDomain":true
+      });
+      chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>ファイルを送信しました。`;
+      window.setTimeout(update, 2000);
+    }catch(e){
+      chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>${e}`;
+      window.setTimeout(() => {
+        chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------`;
+        file_sender();
+      }, 5000);
+    }
+    //window.setTimeout(update, 2000);
   });
   if(file){
-    reader.readAsDataURL(file);
+    if(file.size < 10000000){
+      reader.readAsDataURL(file);
+    }else{
+      chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>ファイルサイズが10MBを超えています。(${Math.round(file.size/100000)/10}MB)`;
+      window.setTimeout(() => {
+        chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------`;
+      }, 5000);
+    }
+  }else{
+    chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>ファイルを選択してください。`;
+    window.setTimeout(() => {
+      chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------`;
+    }, 5000);
   }
 }
 
-async function image_send(){
-  chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>画像送信中...`;
-  if(img_selected){
-    try{
-      let canvas = document.createElement("canvas");
-      let img_src = document.getElementById("img_src");
-      let img_comment = document.getElementById("comment_text").value;
-      canvas.width = img_src.width;
-      canvas.height = img_src.height;
-      let ctx = canvas.getContext("2d");
-      ctx.drawImage(img_src,0,0,img_src.width,img_src.height);
-      let img_base64 = canvas.toDataURL("image/png").substr(22);
-      let img_str = atob(img_base64);
-      encrypted_img_str = encrypt(img_str,log_pass);
-      let log_json = await read(log_path);
-      let room_json = await read("room.json");
-      if(img_comment == ""){
-        log_json[encrypted_account_name]["log"].push(encrypt(`<PATH_OF_IMG>${room_json["writed_images"]}`,log_pass));
-      }else{
-        log_json[encrypted_account_name]["log"].push(encrypt(`<PATH_OF_IMG>${room_json["writed_images"]}<COMMENT>${img_comment}`,log_pass));
-      }
-      log_json[encrypted_account_name]["time"].push(Math.round((new Date()).getTime() / 1000));
-      log_json[encrypted_account_name]["readed"].push([]);
-      room_json["writed_images"]++;
-      await write([log_path,"room.json",`${room_json["writed_images"]-1}.txt`],[JSON.stringify(log_json,undefined,2),JSON.stringify(room_json,undefined,2),encrypted_img_str],3);
-      
-      console.log("image send success.");
-        chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>送信しました。`;
-    }catch(e){
-      chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>送信エラー:${e}`;
-    }
-    reset_sendArea();
-  }else{
-    chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>画像を選んでください。`;
-  }
-  window.setTimeout(update, 2000);
+function DataUrl_sep(uri){
+  let sep = uri.split(',');
+  sep[0] = sep[0].substring(5,sep[0].indexOf(";"));
+  return sep;
+}
+
+function wait_select(mes){
+  return new Promise(async (resolve,reject) => {
+    sendArea.innerHTML = `<p>${mes}</p><input type="button" id="send_button" value="送信"><input type="button" id="cancel_button" value="取消">`;
+    document.getElementById("send_button").addEventListener("click",() => {
+      resolve("send");
+    });
+    document.getElementById("cancel_button").addEventListener("click",() => {
+      resolve("cancel");
+    });
+  });
+}
+
+function wait_load_img(img){
+  return new Promise((resolve,reject) => {
+    img.addEventListener("load", () => {
+      resolve();
+    });
+  });
+}
+
+function wait_load_video(video){
+  return new Promise((resolve,reject) => {
+    video.addEventListener("loadeddata", () => {
+      resolve();
+    });
+  });
 }
 
 function reset_sendArea(){
   sendArea.removeAttribute("hidden");
-  sendArea.innerHTML = '<textarea cols="60" rows="10" wrap="off" id="send_text"></textarea><br><input type="button" id="send_button" value="送信"><input type="button" id="image_button" value="画像" disabled><input type="button" id="icon_button" value="アイコン変更" disabled><input type="button" id="retry_button" value="再読み込み">';
+  sendArea.innerHTML = '<textarea cols="60" rows="10" wrap="off" id="send_text"></textarea><br><input type="button" id="send_button" value="送信"><input type="button" id="file_button" value="ファイル"><input type="button" id="icon_button" value="アイコン変更"><input type="button" id="retry_button" value="再読み込み">';
   document.getElementById("send_button").addEventListener("click",send);
-  document.getElementById("image_button").addEventListener("click",image_sender);
+  document.getElementById("file_button").addEventListener("click",file_sender);
   document.getElementById("icon_button").addEventListener("click",icon_updater);
   document.getElementById("retry_button").addEventListener("click",() => {update(false)});
 }
@@ -194,12 +277,57 @@ async function update(sendArea_update = true){//チャットの更新
     user_icon.src = profiles[log_mes.user].icon;
     user_icon.height = 30;
     comment.appendChild(user_icon);
+    let head_label = document.createElement("font");
+    head_label.className = "FontAAB";
+    head_label.innerHTML = `${date.toLocaleDateString()} ${date.toLocaleTimeString()} ${profiles[log_mes.user].name}>><br>`;
+    comment.appendChild(head_label);
     switch(log_mes.type){
       case "text":
         let comment_label = document.createElement("font");
         comment_label.className = "FontAAB";
-        comment_label.innerHTML = `${date.toLocaleDateString()} ${date.toLocaleTimeString()} ${profiles[log_mes.user].name}>><br>${log_mes.text.replace(/\r?\n/g, '<br>')}`;
+        comment_label.innerHTML = log_mes.text.replace(/\n/g,'<br>');
         comment.appendChild(comment_label);
+        break;
+      case "sticker":
+        let sticker = document.createElement("img");
+        sticker.src = log_mes.src;
+        comment.appendChild(sticker);
+        break;
+      case "file":
+        switch(log_mes.mime){
+          case "image/jpeg":
+            let img = document.createElement("img");
+            img.src = log_mes.src;
+            await wait_load_img(img);
+            var x = Math.sqrt(160000/(img.width*img.height));
+            img.width = Math.floor(x*img.width);
+            comment.appendChild(img);
+            break;
+          case "video/mp4":
+            let video = document.createElement("video");
+            video.src = log_mes.src;
+            await wait_load_video(video);
+            var x = Math.sqrt(160000/(video.videoWidth*video.videoHeight));
+            video.width = Math.floor(x*video.videoWidth);
+            comment.appendChild(video);
+            break;
+          case "audio/aac":
+          case "audio/x-m4a":
+            let audio = document.createElement("audio");
+            audio.src = log_mes.src;
+            audio.controls = true;
+            comment.appendChild(audio);
+            break;
+          default:
+            let button = document.createElement("button");
+            button.innerText = log_mes.name;
+            button.className = "filelink";
+            button.addEventListener("click", () => {
+              location.href = log_mes.src;
+            });
+            comment.appendChild(button);
+        }
+        break;
     }
     chatArea.appendChild(comment);
   }
@@ -238,23 +366,12 @@ async function icon_update(){
       chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>アイコン送信中...`;
       let canvas = document.createElement("canvas");
       let img_src = document.getElementById("img_src");
-      canvas.width = 40;
-      canvas.height = 40;
+      canvas.width = 100;
+      canvas.height = 100;
       let ctx = canvas.getContext("2d");
-      ctx.drawImage(img_src,0,0,40,40);
+      ctx.drawImage(img_src,0,0,100,100);
       let img_base64 = canvas.toDataURL("image/png").substr(22);
-      let img_str = atob(img_base64);
-      encrypted_img_str = encrypt(img_str,log_pass);
-      let log_json = await read(log_path);
-      if(log_json[encrypted_account_name]["icon"] == "none"){
-        let room_json = await read("room.json");
-        log_json[encrypted_account_name]["icon"] = String(room_json["writed_images"]);
-        room_json["writed_images"]++;
-        await write([`${room_json["writed_images"] - 1}.txt`,log_path,"room.json"],[encrypted_img_str,JSON.stringify(log_json,undefined,2),JSON.stringify(room_json,undefined,2)],3);
-      }else{
-        await write(`${log_json[encrypted_account_name]["icon"]}.txt`,encrypted_img_str);
-      }
-      
+      await request({"use":"icon_update","user_id":account_id,"icon":img_base64,"pass":main_pass});
       console.log("image send success.");
       chat_label.innerHTML = `------------------------------<br>${room}<br>------------------------------<br>変更しました。`;
     }catch(e){
@@ -323,51 +440,6 @@ async function get_profile(id){
   return JSON.parse(await request({"use":"profile","id":id,"pass":main_pass,"crossDomain":true}));
 }
 
-function read(path,json_parse = true){
-  return new Promise((resolve,reject) => {
-    if(path in downloaded_images){
-      resolve(downloaded_images[path]);
-    }else{
-      $.post(url,{"use":"read","path":path,"pass":main_pass,"crossDomain":true},data => {
-        if(json_parse){
-          resolve(JSON.parse(data));
-        }else{
-          resolve(data);
-        }
-        if(path.substr(-3,3) == "txt"){
-          downloaded_images[path] = data;
-        }
-      });
-    }
-  });
-}
-
-function write(path,src){
-  return new Promise(async (resolve,reject) => {
-    if(typeof path == "string"){
-      path = [path];
-      src = [src];
-    }
-
-    for(let i = 0;i<path.length;i++){
-      await one_write(path[i],src[i]);
-    }
-    console.log("write success");
-    resolve("write success");
-    if(path.includes(log_path)){
-      dataConnection.send(`<update>${log_path}`);
-    }
-  });
-};
-
-function one_write(path,src){
-  return new Promise((resolve,reject) => {
-    $.post(url,{"use":"write","path":path,"src":src,"pass":main_pass,"crossDomain":true},data => {
-      resolve(data);
-    });
-  });
-}
-
 let encrypted_mainpass = [
   "U2FsdGVkX18SsZ7NI6YcVfLnF0AFxdzk3ihxOUV4f4ozac7vp3hrvudJ7heDQ2xD/1cO3nZvhs3aCsgZGmwSFg==",
   "U2FsdGVkX1/FvkQDrTV0u56M2YTu+jQE6//v3/0SgtCSGiiQAVIJWD3F4JsY0+duVGnZCNCheAaTOZ55ctxWDw==",
@@ -378,8 +450,6 @@ let encrypted_accounts = [
   "U2FsdGVkX1/Dnsss8r32TJsQfau8iPweBezIEBsIvEc=",
   "U2FsdGVkX19PnhZYsYazIjUK0IYiH2nw6btt4uyIHfk="
 ]
-let encrypted_token = "U2FsdGVkX18oKVdt4c+4NOyGVaaRy8uXMnx6aR2RiWf3j7Oz/aCVbohqdQFxxpYvDqJatwgX2Q1FKFM52ZPAZJmKXcZknuN/iA75oozwky61mGnKHefhsZsZw5JYtmiKJe3aKkgZkydavuFbeD6f5zJ+TyS8V6RmP6+7F32fLilqE7iTvVn0y4Vy4ecJA5mnupwk/+HYXWmuEvvOhxuQ8SuhDEddd0pu/dvWk/Fy5JFC63WbIJDm/gHVTO3Z/eBo";
-let token = "";
 let main_pass = "";
 let textArea = document.getElementById("textArea");//ステータスエリア
 let destArea = document.getElementById('destArea');//送信先エリア
